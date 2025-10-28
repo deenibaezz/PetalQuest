a3d963fbb56177f2f37f033644765d1ae877a4fd
Deeni Baez <dbaezflores0699@sdsu.edu>
2025-10-28 00:54:35 -0700

    feat: beetle stomp/side damage logic

--- Assets/Scripts/Enemies/BeetleHurtbox.cs
+++ Assets/Scripts/Enemies/BeetleHurtbox.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class BeetleHurtbox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float stompBounce = 12f;
+    Rigidbody2D beetleRb;
+
+    void Awake(){
+        beetleRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter2D(Collision2D c){
+        if (!c.collider.CompareTag("Player")) return;
+
+        // get references
+        var player = c.collider.GetComponent<PlayerHealth>();
+        var playerRb = c.collider.GetComponent<Rigidbody2D>();
+        if (!player || !playerRb) return;
+
+        // figure out contact: above or side?
+        // Use the first contact normal and positions as a robust check
+        ContactPoint2D contact = c.GetContact(0);
+        Vector2 normal = contact.normal; // normal points from beetle -> player
+
+        bool playerComingDown = playerRb.velocity.y <= -0.05f;
+        bool playerCenterIsAbove = c.collider.bounds.center.y > transform.bounds().center.y + 0.05f;
+
+        // If contact normal mostly points DOWN (player landing on beetle) OR player above & falling
+        bool stomp = (normal.y < -0.5f) || (playerComingDown && playerCenterIsAbove);
+
+        if (stomp){
+            // bounce player up
+            playerRb.velocity = new Vector2(playerRb.velocity.x, stompBounce);
+            // destroy beetle
+            Destroy(gameObject);
+        } else {
+            // damage player & knock them away from beetle
+            Vector2 away = (c.collider.transform.position.x < transform.position.x) ? Vector2.left : Vector2.right;
+            player.TakeDamage(1, away);
+        }
+    }
+}
+
+// handy bounds extension so we can use transform.bounds()
+static class BoundsExt {
+    public static Bounds bounds(this Transform t){
+        var r = t.GetComponent<Renderer>();
+        if (r) return r.bounds;
+        var c = t.GetComponent<Collider2D>();
+        if (c) return c.bounds;
+        return new Bounds(t.position, Vector3.zero);
     }
 }
